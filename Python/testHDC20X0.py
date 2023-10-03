5cff984b04fcecebd448bcad6833a9995bdb5744
pmahiou <pierre.mahiou@mirega.fr>
2023-10-03 08:53:29 +0000

    updated python example file

--- Python/testHDC20X0.py
+++ Python/testHDC20X0.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
 from __future__ import print_function
 
+import time
 import hdc20x0
 
 
 hdc20x0 = hdc20x0.Hdc20x0()
-
+print("Sensor initialized")
 print("{ temperature : %3.1f, humidity : %3.1f }" % (hdc20x0.readTemperature(), hdc20x0.readHumidity()))
 
+print("Retrievieng a few values for control:")
+for i in range(0, 5):
+    print("{ temperature : %3.1f, humidity : %3.1f }" % (hdc20x0.readTemperature(), hdc20x0.readHumidity()))
+    time.sleep(1)
+    
+print("Setting heater on")
+hdc20x0.turnHeaterOn()
+
+print("Temperature should climb up:")
+for i in range(0, 5):
+    print("{ temperature : %3.1f, humidity : %3.1f }" % (hdc20x0.readTemperature(), hdc20x0.readHumidity()))
+    time.sleep(0.5)
+    
+print("Setting heater off")
+hdc20x0.turnHeaterOff()
+
+print("Temperature should go down:")
+for i in range(0, 5):
+    print("{ temperature : %3.1f, humidity : %3.1f }" % (hdc20x0.readTemperature(), hdc20x0.readHumidity()))
+    time.sleep(0.5)
