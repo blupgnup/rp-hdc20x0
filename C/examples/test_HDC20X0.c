db478dd32f81f7fee7252603bb136d061f53bd55
pmahiou <pierre.mahiou@mirega.fr>
2023-10-03 12:20:38 +0000

    updated example with heating test

--- C/examples/test_HDC20X0.c
+++ C/examples/test_HDC20X0.c
@@ -7,12 +7,38 @@ int main()
 	float temperature, humidity; 
 
 	if(setup_hdc20x0() != 0 ) return 0; 
-
-	for (int i = 0; i < 10; i++) {
+    
+	for (int i = 0; i < 5; i++) {
 		if(read_from_hdc20x0(&temperature, &humidity)!=0) return 0;
 
 		printf("{ \"temperature\" : %.1f, \"humidity\" : %.1f }\n", temperature, humidity);
-		sleep(2);
+		sleep(1);
+	}
+    
+    if(turn_heater_on() != 0 ) {
+        printf("Unable to turn heater on");
+        return -3;
+    }
+    
+    printf("Turned heater on. Temperature should climb up:\n");
+    for (int i = 0; i < 5; i++) {
+		if(read_from_hdc20x0(&temperature, &humidity)!=0) return 0;
+
+		printf("{ \"temperature\" : %.1f, \"humidity\" : %.1f }\n", temperature, humidity);
+		sleep(1);
+	}
+    
+    if(turn_heater_off() != 0 ) {
+        printf("Unable to turn heater on");
+        return -3;
+    }    
+    
+    printf("Turned heater off. Temperature should drop off:\n");
+    for (int i = 0; i < 5; i++) {
+		if(read_from_hdc20x0(&temperature, &humidity)!=0) return 0;
+
+		printf("{ \"temperature\" : %.1f, \"humidity\" : %.1f }\n", temperature, humidity);
+		sleep(1);
 	}
 
 	return 0;
